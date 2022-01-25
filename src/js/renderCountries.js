6dc4bb9bd00892cd76d164621da2bd50d3927c2f
Roman <77742141+rumpear@users.noreply.github.com>
2022-01-25 14:24:39 +0200

    add templates

--- src/js/renderCountries.js
+++ src/js/renderCountries.js
@@ -1,6 +1,9 @@
+import Notiflix from 'notiflix';
+
 import { cleanupRender } from './cleanupRender';
 import { refs } from './refs';
-import Notiflix from 'notiflix';
+import countriesListTpl from '../templates/countries-list.hbs';
+import countryCardTpl from '../templates/country-card.hbs';
 
 export function renderCountries(countries) {
   if (countries.length > 10) {
@@ -10,16 +13,19 @@ export function renderCountries(countries) {
   if (countries.length >= 2 && countries.length <= 10) {
     cleanupRender();
 
-    const markupList = countries
-      .map(
-        ({ flags, name }) => `
-            <li class='country-list__item'>
-                <img class='country-list__flag' src="${flags.svg}" alt="${name.official}">
-                <h1 class='country-list__head'>${name.official}</h1>
-            </li>
-        `,
-      )
-      .join('');
+    // const markupList = countries
+    //   .map(
+    //     ({ flags, name }) => `
+    //         <li class='country-list__item'>
+    //             <img class='country-list__flag' src="${flags.svg}" alt="${name.official}">
+    //             <h1 class='country-list__head'>${name.official}</h1>
+    //         </li>
+    //     `,
+    //   )
+    //   .join('');
+
+    const markupList = countries.map(countriesListTpl).join('');
+    console.log(markupList);
 
     refs.listCountry.insertAdjacentHTML('afterbegin', markupList);
   }
@@ -27,19 +33,21 @@ export function renderCountries(countries) {
   if (countries.length === 1) {
     cleanupRender();
 
-    const markupDiv = countries
-      .map(
-        ({ name, capital, population, flags, languages }) => `
-            <div class='country-card__wrapper'> 
-                <img class='country-card__flag' src="${flags.svg}" alt="${name.official}">    
-                <h1>${name.official}</h1>
-            </div>   
-            <h2> Capital: ${capital}</h2>
-            <p> Population: ${population}</p>
-            <p>Official languages: ${Object.values(languages)}</p>
-        `,
-      )
-      .join('');
+    // const markupDiv = countries
+    //   .map(
+    //     ({ name, capital, population, flags, languages }) => `
+    //         <div class='country-card__wrapper'>
+    //             <img class='country-card__flag' src="${flags.svg}" alt="${name.official}">
+    //             <h1>${name.official}</h1>
+    //         </div>
+    //         <h2> Capital: ${capital}</h2>
+    //         <p> Population: ${population}</p>
+    //         <p>Official languages: ${Object.values(languages)}</p>
+    //     `,
+    //   )
+    //   .join('');
+
+    const markupDiv = countries.map(countryCardTpl).join('');
 
     refs.infoCountry.insertAdjacentHTML('afterbegin', markupDiv);
   }
